ac87eee4133a7b651e9d3a38d26faf12856450e7
mericg <meric@upudate.com>
2020-12-20 15:14:28 +0300

    Util class updated.

--- app/src/main/java/com/huawei/sampleappgallery/util/Util.kt
+++ app/src/main/java/com/huawei/sampleappgallery/util/Util.kt
@@ -1,7 +1,6 @@
 package com.huawei.sampleappgallery.util
 
 import android.content.Context
-import android.util.Log
 import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.GoogleApiAvailability
 import com.huawei.hms.api.HuaweiApiAvailability
@@ -9,18 +8,16 @@ import com.huawei.hms.api.HuaweiApiAvailability
 object Util {
 
     fun isHmsAvailable(context: Context): Boolean {
-        var isAvailable = false
+        val isAvailable :Boolean
         val result: Int = HuaweiApiAvailability.getInstance().isHuaweiMobileServicesAvailable(context)
         isAvailable = com.huawei.hms.api.ConnectionResult.SUCCESS == result
-        Log.d("RegisterActivity", "isHmsAvailable: $isAvailable")
         return isAvailable
     }
 
     fun isGmsAvailable(context: Context): Boolean {
-        var isAvailable = false
+        val isAvailable :Boolean
         val result = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(context)
         isAvailable = ConnectionResult.SUCCESS == result
-        Log.d("RegisterActivity", "isGmsAvailable: $isAvailable")
         return isAvailable
     }
 }
